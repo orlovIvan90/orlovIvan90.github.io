a647c6240128e0f3d4a2f125c15116259cb8c008
orlovIvan90 <orlov.i.v.90@gmail.com>
2016-10-25 23:28:15 +0400

    start

---
+++ js/script.js
@@ -0,0 +1,18 @@
+(function($) {
+    $(document).ready(function() {
+        
+        $(".carousel-1").slick({
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              autoplay: false,
+              autoplaySpeed: 4000,
+              adaptiveHeight: false,
+              prevArrow: '<div class="left-wrap"><a class="left carousel-control"></a></div>',
+              nextArrow: '<div class="right-wrap"><a class="right carousel-control"></a></div>',
+              responsive: [{
+                  breakpoint: 736,
+                  settings: "unslick"}]
+        });
+        
+    });
+})(jQuery);
