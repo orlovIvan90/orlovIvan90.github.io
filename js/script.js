3f4692fea59d280169d7079083f97e13ea5c187e
orlovIvan90 <orlov.i.v.90@gmail.com>
2016-10-26 23:11:39 +0400

    done

--- js/script.js
+++ js/script.js
@@ -14,5 +14,30 @@
                   settings: "unslick"}]
         });
         
+        $(".carousel-2").slick({
+              slidesToShow: 5,
+              slidesToScroll: 1,
+              autoplay: false,
+              autoplaySpeed: 4000,
+              adaptiveHeight: false,
+              prevArrow: '<div class="left-wrap"><a class="left carousel-control"></a></div>',
+              nextArrow: '<div class="right-wrap"><a class="right carousel-control"></a></div>',
+              responsive: [{
+                  breakpoint: 736,
+                  settings: "unslick"}]
+        });
+        
+        $(".carousel-3").slick({
+              slidesToShow: 5,
+              slidesToScroll: 1,
+              autoplay: false,
+              autoplaySpeed: 4000,
+              adaptiveHeight: false,
+              prevArrow: '<div class="left-wrap"><a class="left carousel-control"></a></div>',
+              nextArrow: '<div class="right-wrap"><a class="right carousel-control"></a></div>',
+              responsive: [{
+                  breakpoint: 736,
+                  settings: "unslick"}]
+        });
     });
 })(jQuery);
