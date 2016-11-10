ed23d6a493cc2b5e77d0fad0b40318e7e3ca484f
orlovIvan90 <orlov.i.v.90@gmail.com>
2016-11-10 23:16:21 +0400

    fix

--- js/script.js
+++ js/script.js
@@ -4,40 +4,31 @@
         $(".carousel-1").slick({
               slidesToShow: 1,
               slidesToScroll: 1,
-              autoplay: false,
+              autoplay: true,
               autoplaySpeed: 4000,
               adaptiveHeight: false,
               prevArrow: '<div class="left-wrap"><a class="left carousel-control"></a></div>',
-              nextArrow: '<div class="right-wrap"><a class="right carousel-control"></a></div>',
-              responsive: [{
-                  breakpoint: 736,
-                  settings: "unslick"}]
+              nextArrow: '<div class="right-wrap"><a class="right carousel-control"></a></div>'
         });
         
         $(".carousel-2").slick({
               slidesToShow: 5,
               slidesToScroll: 1,
-              autoplay: false,
+              autoplay: true,
               autoplaySpeed: 4000,
               adaptiveHeight: false,
               prevArrow: '<div class="left-wrap"><a class="left carousel-control"></a></div>',
-              nextArrow: '<div class="right-wrap"><a class="right carousel-control"></a></div>',
-              responsive: [{
-                  breakpoint: 736,
-                  settings: "unslick"}]
+              nextArrow: '<div class="right-wrap"><a class="right carousel-control"></a></div>'
         });
         
         $(".carousel-3").slick({
               slidesToShow: 5,
               slidesToScroll: 1,
-              autoplay: false,
+              autoplay: true,
               autoplaySpeed: 4000,
               adaptiveHeight: false,
               prevArrow: '<div class="left-wrap"><a class="left carousel-control"></a></div>',
-              nextArrow: '<div class="right-wrap"><a class="right carousel-control"></a></div>',
-              responsive: [{
-                  breakpoint: 736,
-                  settings: "unslick"}]
+              nextArrow: '<div class="right-wrap"><a class="right carousel-control"></a></div>'
         });
     });
 })(jQuery);
