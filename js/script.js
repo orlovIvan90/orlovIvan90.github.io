5d2d9d0b28207a65bb0c822dae383d3b8e1f50e4
orlovIvan90 <orlov.i.v.90@gmail.com>
2016-11-14 23:16:01 +0400

    fix

--- js/script.js
+++ js/script.js
@@ -2,5 +2,14 @@
     $(document).ready(function() {
         
         $('input, select').styler();
+        /*
+        $(".screen-4__button").on("focusin", function() {
+            $(".button-text").css("margin-top", "-13px");
+        });
+        
+        $(".screen-4__button").on("focusout", function() {
+            $(".button-text").css("margin-top", "-20px");
+        });
+        */
     });
 })(jQuery);
