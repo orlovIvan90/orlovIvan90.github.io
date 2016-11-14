167d31d433534ec5d097789120df48f1ce486ee6
orlovIvan90 <orlov.i.v.90@gmail.com>
2016-11-14 17:35:05 +0400

    fix

--- js/script.js
+++ js/script.js
@@ -1,34 +1,6 @@
 (function($) {
     $(document).ready(function() {
         
-        $(".carousel-1").slick({
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              autoplay: true,
-              autoplaySpeed: 4000,
-              adaptiveHeight: false,
-              prevArrow: '<div class="left-wrap"><a class="left carousel-control"></a></div>',
-              nextArrow: '<div class="right-wrap"><a class="right carousel-control"></a></div>'
-        });
         
-        $(".carousel-2").slick({
-              slidesToShow: 5,
-              slidesToScroll: 1,
-              autoplay: true,
-              autoplaySpeed: 4000,
-              adaptiveHeight: false,
-              prevArrow: '<div class="left-wrap"><a class="left carousel-control"></a></div>',
-              nextArrow: '<div class="right-wrap"><a class="right carousel-control"></a></div>'
-        });
-        
-        $(".carousel-3").slick({
-              slidesToShow: 5,
-              slidesToScroll: 1,
-              autoplay: true,
-              autoplaySpeed: 4000,
-              adaptiveHeight: false,
-              prevArrow: '<div class="left-wrap"><a class="left carousel-control"></a></div>',
-              nextArrow: '<div class="right-wrap"><a class="right carousel-control"></a></div>'
-        });
     });
 })(jQuery);
