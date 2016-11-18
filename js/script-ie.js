8ad01727f6ffb9e5a01066143b2eafbe228cfe81
orlovIvan90 <orlov.i.v.90@gmail.com>
2016-11-18 12:59:14 +0400

    fix

--- js/script-ie.js
+++
@@ -1,7 +0,0 @@
-window.attachEvent('onload', mmwidth);  
-window.attachEvent('onresize', mmwidth);  
-function mmwidth(){  
-	document.getElementById('wrapper').style.width = ((document.documentElement.clientWidth ||  
-	document.body.clientWidth) < 1002) ? '1002px' :  
-	((document.body.clientWidth > 1002) ? 'auto' : 'auto');
-};
