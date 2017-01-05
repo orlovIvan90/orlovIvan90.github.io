c3ffa1e0a732825d849f9481972e38200b5dc695
Ivan Orlov <orlov.i.v.90@gmail.com>
2017-01-05 19:33:10 +0400

    test 6

--- www.aft.ru/js/script-ie.js
+++
@@ -1,8 +0,0 @@
-window.attachEvent('onload', mmwidth);  
-window.attachEvent('onresize', mmwidth);  
-function mmwidth(){  
-	document.getElementById('main-wrapper').style.width = ((document.documentElement.clientWidth ||  
-	document.body.clientWidth) < 1000) ? '1000px' :  
-	((document.body.clientWidth > 1000) ? 'auto' : 'auto');
-};
-DD_belatedPNG.fix('#play_button');
