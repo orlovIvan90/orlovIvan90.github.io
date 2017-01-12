4e406a17ca851e54a8728c12ea6244932be3a839
Ivan Orlov <orlov.i.v.90@gmail.com>
2017-01-12 15:02:38 +0400

    brcmnd new

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 (function($) {
     $(document).ready(function() {
-          $( "#tabs" ).tabs({
-              active: 1
-          });
+        var video = document.getElementById("video"); 
+        
+        $("#content__button").on("click", function(e) {
+            e.preventDefault();
+            
+            $("#content__title-1").css("display", "none");
+            $("#content__title-2").css("display", "block");
+            
+            $("#content__caption-1").css("display", "none");
+            $("#content__caption-2").css("display", "block");
+            
+            $("#content__form").css("display", "none");
+            
+            $("#footer__caption").css("display", "none");
+            
+            $("#social").css("display", "block");
+            
+            video.play();
+        });
+        
     });
+        
 })(jQuery);
