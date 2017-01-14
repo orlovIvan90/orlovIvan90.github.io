47e9b084b422834804ce604294a688e7dcf43b9c
Ivan Orlov <orlov.i.v.90@gmail.com>
2017-01-14 11:25:24 +0400

    new remarks done

--- js/script.js
+++ js/script.js
@@ -1,25 +1,49 @@
 (function($) {
     $(document).ready(function() {
-        var video = document.getElementById("video"); 
-        
-        $("#content__button").on("click", function(e) {
-            e.preventDefault();
+//        var video = document.getElementById("video"); 
+//            video.play();
+        var isShown = false;
+
+        $("#content__logo").on("click", function(e) {
+            if (isShown) {
+                e.preventDefault();
             
-            $("#content__title-1").css("display", "none");
-            $("#content__title-2").css("display", "block");
-            
-            $("#content__caption-1").css("display", "none");
-            $("#content__caption-2").css("display", "block");
-            
-            $("#content__form").css("display", "none");
-            
-            $("#footer__caption").css("display", "none");
-            
-            $("#social").css("display", "block");
-            
-            video.play();
+                $("#content__title-1").css("display", "block");
+                $("#content__title-2").css("display", "none");
+
+                $("#content__caption-1").css("display", "block");
+                $("#content__caption-2").css("display", "none");
+
+                $("#content__form").css("display", "block");
+
+                $("#footer__caption").css("display", "block");
+
+                $("#social").css("display", "none");
+                
+                isShown = false;
+            } 
         });
         
+        $.validate({
+            modules : 'security',
+            onSuccess : function($form) {
+                
+                $("#content__title-1").css("display", "none");
+                $("#content__title-2").css("display", "block");
+
+                $("#content__caption-1").css("display", "none");
+                $("#content__caption-2").css("display", "block");
+
+                $("#content__form").css("display", "none");
+
+                $("#footer__caption").css("display", "none");
+
+                $("#social").css("display", "block");
+                
+                isShown = true;
+
+            }
+        });
     });
         
 })(jQuery);
