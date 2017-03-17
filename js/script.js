4fb0548cef744aa0553a371b640fbce1a7e69a92
Ivan Orlov <orlov.i.v.90@gmail.com>
2017-03-17 14:49:59 +0400

    fix

--- js/script.js
+++ js/script.js
@@ -110,8 +110,6 @@
 
             var scene8 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#master__animation-trigger", duration: "60%"}).setTween(masterPink1).addTo(controller);
             
-
-            
             
             var masterPink2 = TweenMax.to("#master__img-wrap-5", 1, {rotation: 45, repeat: 2, yoyo: true, ease: Linear.easeNone});
             
@@ -125,97 +123,24 @@
             
             var masterPink3 = TweenMax.to("#master__img-wrap-6", 1, { opacity: 1, top: "50px", ease: Linear.easeNone});
 
-            var scene8 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#master__animation-trigger", duration: "100%"}).setTween(masterPink3).addTo(controller);
-//
-//
-//
-//            var do__green3 = TweenMax.to("#do__green3", 1, { opacity: 1, y: "-100", ease: Linear.easeNone});
-//
-//            var scene5 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#do__green3-trigger", duration: "30%"}).setTween(do__green3).addTo(controller);
-//
-//
-//            var do__text3 = TweenMax.to("#do__text3", 1, { opacity: 1, y: "-100", ease: Linear.easeNone});
-//
-//            var scene6 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#do__text3-trigger", duration: "40%"}).setTween(do__text3).addTo(controller);
-//
-//
-//
-//            var do__green4 = TweenMax.to("#do__green4", 1, { opacity: 1, y: "-100", ease: Linear.easeNone});
-//
-//            var scene7 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#do__green4-trigger", duration: "30%"}).setTween(do__green4).addTo(controller);
-//
-//
-//            var do__text4 = TweenMax.to("#do__text4", 1, { opacity: 1, y: "-100", ease: Linear.easeNone});
-//
-//            var scene8 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#do__text4-trigger", duration: "40%"}).setTween(do__text4).addTo(controller);
-//
-//
-//
-//            var do__green5 = TweenMax.to("#do__green5", 1, { opacity: 1, y: "-100", ease: Linear.easeNone});
-//
-//            var scene9 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#do__green5-trigger", duration: "30%"}).setTween(do__green5).addTo(controller);
-//
-//
-//            var do__text5 = TweenMax.to("#do__text5", 1, { opacity: 1, y: "-100", ease: Linear.easeNone});
-//
-//            var scene10 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#do__text5-trigger", duration: "40%"}).setTween(do__text5).addTo(controller);
-//            /* -------------- */    
-//            
-//            
-//            
-//            /* community animation */
-//            var community__text1 = TweenMax.to("#community__text1", 1, { opacity: 1, y: "-200", ease: Linear.easeNone});
-//
-//            var scene11 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#community__text1-trigger", duration: "20%"}).setTween(community__text1).addTo(controller);
-//
-//
-//            var community__text2 = TweenMax.to("#community__text2", 1, { opacity: 1, y: "-200", ease: Linear.easeNone});
-//
-//            var scene12 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#community__text2-trigger", duration: "20%"}).setTween(community__text2).addTo(controller);
-//
-//
-//            var community__text3 = TweenMax.to("#community__text3", 1, { opacity: 1, y: "-200", ease: Linear.easeNone});
-//
-//            var scene13 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#community__text3-trigger", duration: "20%"}).setTween(community__text3).addTo(controller);
-//
-//
-//            var community__text4 = TweenMax.to("#community__text4", 1, { opacity: 1, y: "-200", ease: Linear.easeNone});
-//
-//            var scene14 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#community__text4-trigger", duration: "20%"}).setTween(community__text4).addTo(controller);
-//
-//
-//            var community__text5 = TweenMax.to("#community__text5", 1, { opacity: 1, y: "-200", ease: Linear.easeNone});
-//
-//            var scene15 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#community__text5-trigger", duration: "20%"}).setTween(community__text5).addTo(controller);
-//
-//
-//            var community__text6 = TweenMax.to("#community__text6", 1, { opacity: 1, y: "-200", ease: Linear.easeNone});
-//
-//            var scene16 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#community__text6-trigger", duration: "20%"}).setTween(community__text6).addTo(controller);
-//            /* -------------- */
-//
-//
-//            /* banner-2 animation */
-//
-//            var banner__wrap2 = TweenMax.to("#banner-wrap2", 1, { opacity: 1, y: "-200", ease: Linear.easeNone});
-//
-//            var scene17 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#banner2__trigger", duration: "80%"}).setTween(banner__wrap2).addTo(controller);
-//
-//
-//
-//            /* banner-3 animation */
-//
-//            var banner__wrap3 = TweenMax.to("#banner-wrap3", 1, { opacity: 1, y: "-200", ease: Linear.easeNone});
-//
-//            var scene18 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#banner3__trigger", duration: "80%"}).setTween(banner__wrap3).addTo(controller);
-//
-//
-//            /* banner-4 animation */
-//
-//            var banner__wrap4 = TweenMax.to("#banner-wrap4", 1, { opacity: 1, y: "-200", ease: Linear.easeNone});
-//
-//            var scene19 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#banner4__trigger", duration: "80%"}).setTween(banner__wrap4).addTo(controller);
+            var scene11 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#master__animation-trigger", duration: "100%"}).setTween(masterPink3).addTo(controller);
+
             
+            var tubes = TweenMax.to("#tubes", 1, { opacity: 1, top: "0px", ease: Linear.easeNone});
+
+            var scene12 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#tubes__trigger", duration: "10%"}).setTween(tubes).addTo(controller);
+            
+            var ad = TweenMax.to("#ad", 1, { opacity: 1, top: "60%", ease: Linear.easeNone});
+
+            var scene13 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#tubes__trigger", duration: "60%"}).setTween(ad).addTo(controller);
+            
+            var adBig = TweenMax.to("#ad--big", 1, { opacity: 1, top: "280px", ease: Linear.easeNone});
+
+            var scene14 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#ad--big__trigger", duration: "100%"}).setTween(adBig).addTo(controller);
+            
+            var scissors = TweenMax.to("#scissors", 1, { opacity: 1, bottom: "150px", ease: Linear.easeNone});
+
+            var scene15 = new ScrollMagic.Scene({triggerHook: "onEnter", triggerElement:"#ad--big__trigger", duration: "70%"}).setTween(scissors).addTo(controller);
         };        
         
         skel.breakpoints({
