de05cb86dd801bfcdc4c3ec025f7d6e579b1afa9
Ivan Orlov <orlov.i.v.90@gmail.com>
2017-04-17 23:03:36 +0400

    onefit started

---
+++ js/script.js
@@ -0,0 +1,25 @@
+(function($) {
+    $(document).ready(function() {
+        
+//        $("#personal__photo").change(function(){
+//             var filename = $(this).val().replace(/.*\\/, "");
+//             $("#personal__result").html(filename);
+//        });
+//        
+//        $("#work__logo").change(function(){
+//             var filename = $(this).val().replace(/.*\\/, "");
+//             $("#work__result").html(filename);
+//        });
+        
+//        $('#check1').styler();
+//        
+//        $("#avatar__sex").styler();
+//        
+//        $("#avatar__city").styler();
+//        
+//        $("#avatar__trade-network").styler();
+//        
+//        $("#avatar__trade-address").styler();
+    });
+        
+})(jQuery);
